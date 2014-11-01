d3b2896bd894e392f7913e566532ba7c8593334b
kan6xing <315030809@qq.com>
2014-11-01 10:29:05 +0800

    :wq

---
+++ BPMCodeGen/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace BPMCodeGen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.richSetup = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.MobTxt = new System.Windows.Forms.TextBox();
+            this.mobBtn = new System.Windows.Forms.Button();
+            this.mobComb = new System.Windows.Forms.ComboBox();
+            this.richMob = new System.Windows.Forms.RichTextBox();
+            this.webB1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "请选择一个模板";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(165, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(288, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "请选择模板";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(39, 59);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox1.Size = new System.Drawing.Size(831, 178);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(515, 23);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(75, 23);
+            this.saveBtn.TabIndex = 4;
+            this.saveBtn.Text = "保存文件";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // richSetup
+            // 
+            this.richSetup.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.richSetup.Location = new System.Drawing.Point(39, 256);
+            this.richSetup.Name = "richSetup";
+            this.richSetup.Size = new System.Drawing.Size(831, 188);
+            this.richSetup.TabIndex = 5;
+            this.richSetup.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(401, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = " 查看效果";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // MobTxt
+            // 
+            this.MobTxt.Location = new System.Drawing.Point(613, 25);
+            this.MobTxt.Name = "MobTxt";
+            this.MobTxt.Size = new System.Drawing.Size(100, 21);
+            this.MobTxt.TabIndex = 7;
+            // 
+            // mobBtn
+            // 
+            this.mobBtn.Location = new System.Drawing.Point(731, 22);
+            this.mobBtn.Name = "mobBtn";
+            this.mobBtn.Size = new System.Drawing.Size(75, 23);
+            this.mobBtn.TabIndex = 8;
+            this.mobBtn.Text = "选择子模板";
+            this.mobBtn.UseVisualStyleBackColor = true;
+            this.mobBtn.Click += new System.EventHandler(this.mobBtn_Click);
+            // 
+            // mobComb
+            // 
+            this.mobComb.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.mobComb.FormattingEnabled = true;
+            this.mobComb.Location = new System.Drawing.Point(850, 25);
+            this.mobComb.Name = "mobComb";
+            this.mobComb.Size = new System.Drawing.Size(121, 20);
+            this.mobComb.TabIndex = 9;
+            this.mobComb.SelectedValueChanged += new System.EventHandler(this.mobComb_SelectedValueChanged);
+            // 
+            // richMob
+            // 
+            this.richMob.Location = new System.Drawing.Point(876, 59);
+            this.richMob.Name = "richMob";
+            this.richMob.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richMob.Size = new System.Drawing.Size(247, 178);
+            this.richMob.TabIndex = 10;
+            this.richMob.Text = "";
+            // 
+            // webB1
+            // 
+            this.webB1.Location = new System.Drawing.Point(39, 470);
+            this.webB1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webB1.Name = "webB1";
+            this.webB1.Size = new System.Drawing.Size(831, 213);
+            this.webB1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1135, 682);
+            this.Controls.Add(this.webB1);
+            this.Controls.Add(this.richMob);
+            this.Controls.Add(this.mobComb);
+            this.Controls.Add(this.mobBtn);
+            this.Controls.Add(this.MobTxt);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richSetup);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.RichTextBox richSetup;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox MobTxt;
+        private System.Windows.Forms.Button mobBtn;
+        private System.Windows.Forms.ComboBox mobComb;
+        private System.Windows.Forms.RichTextBox richMob;
+        private System.Windows.Forms.WebBrowser webB1;
+    }
+}
+
---
+++ BPMCodeGen/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace BPMCodeGen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            OpenFileDialog open = new OpenFileDialog();
+            //open.Filter = "Word文档(*.doc)|*.doc|其它文档(*.*)|*.*";
+            open.Filter = "所有文档(*.*)|*.*";
+            if(open.ShowDialog()==DialogResult.OK)
+            {
+                this.textBox1.Text = open.FileName;
+                FileStream fs = new FileStream(open.FileName, FileMode.Open);
+                StreamReader sr = new StreamReader(fs,Encoding.Default);
+
+                string allStr = sr.ReadToEnd();
+                int startInt=0;
+                startInt= allStr.IndexOf("$[", startInt);
+
+                string TestStr = "";
+                while(startInt>0)
+                {
+                    string subStr= allStr.Substring(startInt, allStr.IndexOf("]", startInt) - startInt+1);
+                    //allStr= allStr.Replace(subStr,"字段"+startInt);
+                    startInt = allStr.IndexOf("$[", startInt+1);
+                    
+                    if(string.IsNullOrEmpty(TestStr))
+                    {
+                        TestStr += subStr + ":\n";
+                    }else
+                    {
+                        TestStr += ";"+subStr + ":\n";
+                    }
+                }
+                this.richTextBox1.Text = TestStr+allStr ;
+                this.richSetup.Text = TestStr.Trim();
+                fs.Flush();
+                sr.Close();
+                fs.Close();
+            }
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sf = new SaveFileDialog();
+            sf.Filter = "文本文件|*.txt";
+            //sf.FilterIndex=2;
+            //sf.RestoreDirectory = true;
+            if(sf.ShowDialog()==DialogResult.OK)
+            {
+                StreamWriter sw = new StreamWriter(sf.FileName, false, Encoding.Default);
+                sw.Write(this.richTextBox1.Text);
+                sw.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(this.richSetup.Text.Trim().Equals(""))
+            {
+                return;
+            }
+            if(string.IsNullOrEmpty(this.textBox1.Text.Trim()))
+            {
+                MessageBox.Show("请选择一个模板");
+                return;
+            }
+
+            string fileName = this.textBox1.Text;
+            FileStream fs = new FileStream(fileName, FileMode.Open);
+            StreamReader sr = new StreamReader(fs, Encoding.Default);
+
+            string allStr = sr.ReadToEnd();
+            int startInt = 0;
+            startInt = allStr.IndexOf("$[", startInt);
+
+            string TestStr = "";
+            while (startInt > 0)
+            {
+                string subStr = allStr.Substring(startInt, allStr.IndexOf("]", startInt) - startInt + 1);
+                
+                string[] strs= this.richSetup.Text.Split(';');
+                foreach(string str in strs)
+                {
+                    string[] props= str.Split(':');
+                    if(subStr.Contains(props[0].Trim().Trim('\n')))
+                    {
+
+                        if (subStr.Contains("$[@"))
+                        {
+                            switch (subStr)
+                            {
+                                case "$[@tr$]":
+                                    //allStr = allStr.Replace(subStr, props[1]+"neibu");
+                                    int colCount=1;
+                                    string[] rowsp= props[1].Split(' ');
+                                    colCount=int.Parse(rowsp[0]);
+                                    string replaceStr = "";
+
+
+                                    for (int i = 1; i < rowsp.Count();i++ )
+                                    {
+                                        //string[] rowsppppp = rowsp[i].Split(new string[] { "*" }, StringSplitOptions.RemoveEmptyEntries);
+                                        //for (int i = 0; i < rowsppppp.Count();i++ )
+                                        //{
+
+                                        //}
+
+                                            if (rowsp[i].Trim().StartsWith("mb"))
+                                            {
+                                                string mbName = rowsp[i].Trim().Substring(0, rowsp[i].IndexOf("["));
+
+                                                foreach (ComboBoxItem item in mobComb.Items)
+                                                {
+                                                    if (item.Text.Equals(mbName))
+                                                    {
+                                                        replaceStr = "<tr>" + item.Value.ToString() + "</tr>";
+                                                        break;
+                                                    }
+                                                }
+
+                                                string submb = subStrSE(rowsp[i], "[", "]");
+                                                if (!string.IsNullOrEmpty(submb))
+                                                {
+                                                    string[] ppsub = submb.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+                                                    foreach (string psub in ppsub)
+                                                    {
+                                                        string[] psmsubs = psub.Split(new string[] { "->" }, StringSplitOptions.RemoveEmptyEntries);
+                                                        replaceStr = replaceStr.Replace("$[" + psmsubs[0] + "$]", psmsubs[1]);
+                                                    }
+                                                }else
+                                                {
+
+                                                }
+
+                                            }
+                                    }
+                                    allStr = allStr.Replace(subStr, replaceStr);
+                                    break;
+                                default:
+                                    break;
+
+                            }
+                            break;
+                        }
+                        
+                        allStr = allStr.Replace(subStr, props[1]);
+                        break;
+                    }
+                }
+                
+                startInt = allStr.IndexOf("$[", 0);
+                TestStr += subStr + "\n";
+            }
+            this.richTextBox1.Text = allStr;
+
+            //fs.Flush();
+            sr.Close();
+            fs.Close();
+        }
+
+        private void mobBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            //open.Filter = "Word文档(*.doc)|*.doc|其它文档(*.*)|*.*";
+            open.Filter = "所有文档(*.*)|*.*";
+            if(open.ShowDialog()==DialogResult.OK)
+            {
+                this.MobTxt.Text = open.FileName;
+
+                FileStream fs = new FileStream(open.FileName, FileMode.Open);
+                StreamReader sr = new StreamReader(fs, Encoding.Default);
+
+                string allStr = sr.ReadToEnd();
+                string[] comStr= allStr.Split(new string[]{"@@"}, StringSplitOptions.RemoveEmptyEntries);
+                foreach(string coms in comStr)
+                {
+                   
+                    string[] ccs = coms.Split(new string[] { "::" }, StringSplitOptions.RemoveEmptyEntries);
+                    this.mobComb.Items.Add(new ComboBoxItem(ccs[0].Trim(new Char[]{'\r','\n'}).Trim(),ccs[1]));
+                }
+
+                sr.Close();
+                fs.Close();
+                
+            }
+        }
+
+        private void mobComb_SelectedValueChanged(object sender, EventArgs e)
+        {
+            this.richMob.Text = ((ComboBoxItem)this.mobComb.SelectedItem).Value.ToString();
+            if(richMob.Text.Trim().StartsWith("<td"))
+            {
+                this.webB1.DocumentText = "<table><tr>"+((ComboBoxItem)this.mobComb.SelectedItem).Value.ToString()+"</tr></table>";
+            }
+            
+        }
+
+        private string subStrSE(string sourceString,string startStr,string endStr,bool isContains=false)
+        {
+            int startInt=sourceString.IndexOf(startStr);
+            if ((sourceString.IndexOf(endStr) - startInt) <= 1)
+                return "";
+            string retStr = "";
+            if(isContains)
+            {
+                retStr = sourceString.Substring(startInt, sourceString.IndexOf(endStr, startInt) - startInt + endStr.Length);
+               
+            }else
+            {
+                retStr = sourceString.Substring(startInt + startStr.Length, sourceString.IndexOf(endStr, startInt) - startInt-1); 
+            }
+            return retStr;
+        }
+    }
+
+    public class ComboBoxItem:IComparable 
+    {
+        public ComboBoxItem(){}
+        public ComboBoxItem(string text,string value)
+        {
+            this._text = text;
+            this._value = value;
+        }
+        private string _text = null;
+        private object _value = null;
+        public string Text { get { return this._text; } set { this._text = value; } }
+        public object Value { get { return this._value; } set { this._value = value; } }
+        public override string ToString()
+        {
+            return this._text;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if(obj is ComboBoxItem)
+            {
+                if (((ComboBoxItem)obj).Text.Equals(this.Text))
+                {
+                    return 0;
+                }
+                else
+                {
+                    return ((ComboBoxItem)obj).Text.CompareTo(this.Text);
+                }
+            }else
+            {
+                if (((string)obj).Equals(this.Text))
+                {
+                    return 0;
+                }
+                else
+                {
+                    return ((string)obj).CompareTo(this.Text);
+                }
+            }
+            
+            
+        }
+    }
+}
